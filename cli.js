3e236cf42c5969c211db5f6a2ee98d0d6202d4e0
Jevgeni Sillar <jevgeni@pitfire.io>
2020-05-17 11:05:43 +0300

    update webz version, update package sample, add tsconfig

--- cli.js
+++ cli.js
@@ -1,35 +1,47 @@
 #!/usr/bin/env node
 
-const indexTs =
-`import { init } from '@khv/webz/config';
+const indexTs = `import 'reflect-metadata';
+import { init } from '@khv/webz/config';
+import { join } from 'path';
 
 init({
-	controllers: './controllers',
-	enableSystemController: true
+  controllers: join(__dirname, 'controllers'),
+  enableSystemController: true,
+  dbConfig: null,
 });
-`
+`;
+
+const tsconfig = `{
+  "extends": "./node_modules/@khv/webz/tsconfig.json",
+  "compilerOptions": {
+    "outDir": "./build",
+    "moduleResolution": "node",
+    "baseUrl": "../src",
+  }
+}
+`;
+
 const { load, run } = require('npm');
 const { basename, resolve, join } = require('path');
 const { writeFile, readFile, mkdir } = require('fs').promises;
 const { existsSync } = require('fs');
 
-const args = require('args')
-	.option('init', 'Initialize a new project')
-	.option('dev', 'Start server');
+const args = require('args').option('init', 'Initialize a new project').option('dev', 'Start server');
 
 const flags = args.parse(process.argv);
 
 if (flags.init) {
-	const p = require('./package.sample.json');
+  const p = require('./package.sample.json');
 
-	p.name = basename(resolve());
+  p.name = basename(resolve());
 
-	writeFile(join(resolve(), 'package.json'), JSON.stringify(p, null, 2), 'utf8')
-		.then(() => existsSync(join(resolve(), 'src')) ? Promise.resolve() : mkdir(join(resolve(), 'src')))
-		.then(f => writeFile(join(resolve(), 'src', 'index.ts'), indexTs, 'utf8'))
-		.catch(err => console.log(err));
+  writeFile(join(resolve(), 'package.json'), JSON.stringify(p, null, 2), 'utf8')
+    .then(() => (existsSync(join(resolve(), 'src')) ? Promise.resolve() : mkdir(join(resolve(), 'src'))))
+    .then((f) => writeFile(join(resolve(), 'src', 'index.ts'), indexTs, 'utf8'))
+    .then((f) => writeFile(join(resolve(), 'tsconfig.json'), tsconfig, 'utf8'))
+    .catch((err) => console.log(err));
 }
 
 if (flags.dev) {
-	load(() => run('dev'));
+  load(() => run('dev'));
 }
